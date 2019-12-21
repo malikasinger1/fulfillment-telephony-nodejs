154f270aea0b183bbbcca429f1b859c7838ece51
Mohammad Inzamam Malik <malikasinger1@gmail.com>
2019-12-22 02:15:02 +0500

    added intent map

--- functions/index.js
+++ functions/index.js
@@ -17,13 +17,13 @@
 'use strict';
 
 const functions = require('firebase-functions');
-const {WebhookClient} = require('dialogflow-fulfillment');
-const {google} = require('googleapis');
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { google } = require('googleapis');
 
 const sheets = google.sheets('v4');
 process.env.DEBUG = 'dialogflow:debug';
 
-const serviceAccount =  { };   // Enter Service Account private key JSON
+const serviceAccount = {};   // Enter Service Account private key JSON
 
 const client = new google.auth.JWT({
   email: serviceAccount.client_email,
@@ -35,40 +35,62 @@ const timeZone = 'America/Los_Angeles';
 const timeZoneOffset = '-07:00';
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-  const agent = new WebhookClient({request, response});
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-  agent.handleRequest(agent => {
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  function bookAppointment(agent) {
     let [date, time, name, group, phone] = [agent.parameters.date, agent.parameters.time,
-                                          agent.parameters.name, agent.parameters.groupN,
-                                          agent.parameters.phone];
+    agent.parameters.name, agent.parameters.groupN,
+    agent.parameters.phone];
 
-    let shortDate = date.slice(0,10);           //converts ISO formatting to readable params
-    let shortTime = time.slice(11,16);
+    let shortDate = date.slice(0, 10);           //converts ISO formatting to readable params
+    let shortTime = time.slice(11, 16);
 
-      // Google Sheets URL: https://docs.google.com/spreadsheets/d/__SPREADSHEETID__/
-      // Go to Google Sheets URL and be sure to share directly with your Service Account client_email 
-      return new Promise((resolve, reject) => {
-        client.authorize((err, tokens) => {
-          const sheets = google.sheets('v4');
+    // Google Sheets URL: https://docs.google.com/spreadsheets/d/__SPREADSHEETID__/
+    // Go to Google Sheets URL and be sure to share directly with your Service Account client_email 
+    return new Promise((resolve, reject) => {
+      client.authorize((err, tokens) => {
+        const sheets = google.sheets('v4');
 
-            sheets.spreadsheets.values.append({
-              auth: client,
-              spreadsheetId: ' ',             // Enter your spreadsheetId here
-              range: 'Sheet1!A1:F1',
-              valueInputOption: 'USER_ENTERED',
-              resource: {
-                values: [
-                  [`${shortDate}`, `${shortTime}`, `${name}`, `${group}`, `${phone}`]
-                ]
-              }
-            }, (e, reservation) => e ? reject(e) : resolve(reservation));
+        sheets.spreadsheets.values.append({
+          auth: client,
+          spreadsheetId: ' ',             // Enter your spreadsheetId here
+          range: 'Sheet1!A1:F1',
+          valueInputOption: 'USER_ENTERED',
+          resource: {
+            values: [
+              [`${shortDate}`, `${shortTime}`, `${name}`, `${group}`, `${phone}`]
+            ]
+          }
+        }, (e, reservation) => e ? reject(e) : resolve(reservation));
 
-        });
-      }).then(() => { response.json({ 'fulfillmentText': `Perfect I've got you down for ${shortDate} at ${shortTime},
+      });
+    }).then(() => {
+      response.json({
+        'fulfillmentText': `Perfect I've got you down for ${shortDate} at ${shortTime},
       see you later!` });
-      }).catch(() => {
-        agent.add(`I'm sorry I'm not able to take down your reservation but you'll be connected to the main line in a moment`);
-        let callEvent = agent.setFollowupEvent('call_transfer_event');
-        });
+    }).catch(() => {
+      agent.add(`I'm sorry I'm not able to take down your reservation but you'll be connected to the main line in a moment`);
+      let callEvent = agent.setFollowupEvent('call_transfer_event');
     });
+  }
+
+
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  intentMap.set('bookAppointment', bookAppointment);
+  // intentMap.set('your intent name here', yourFunctionHandler);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
 });
